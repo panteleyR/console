3ba315f263eb45ed90284aeab7fe03c61736d86a
panteleyR <panteley98@bk.ru>
2022-10-08 23:24:18 +0400

    updated

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,18 +4,20 @@ declare(strict_types=1);
 
 namespace Lilith\Console;
 
-use Lilith\DependencyInjection\ContainerInterface;
+use Throwable;
 
 class Kernel implements KernelInterface
 {
-    public function __construct(protected ContainerInterface $container) {}
+    public function __construct(protected readonly RouterInterface $router) {}
 
+    /**
+     * @throws Throwable
+     */
     public function handle(ArgvInput $argvInput): void
     {
         try {
-            $router = $this->container->get('console.router');
-            $router->execute($argvInput);
-        } catch (\Throwable $e) {
+            $this->router->execute($argvInput);
+        } catch (Throwable $e) {
             throw $e;
         }
     }
